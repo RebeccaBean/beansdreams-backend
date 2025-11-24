209614041ec8c85f27b16a40751c8f41f5714199
RebeccaBean <nickrebbean@gmail.com>
2025-11-23 22:15:47 -0800

    Delete extra files

--- test-env.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-console.log("Firebase key:", process.env.FIREBASE_SERVICE_ACCOUNT_KEY?.slice(0, 50));
-console.log("PayPal client:", process.env.PAYPAL_CLIENT_ID?.slice(0, 20));
-import dotenv from "dotenv";
-dotenv.config();
-
-console.log("Keys loaded:", Object.keys(process.env));
